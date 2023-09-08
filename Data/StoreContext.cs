ad0be205189c54f56cb1c2272e7f9b6fc56c6f4e
Victor Fuentes <vicuo@hotmail.com>
2023-09-08 12:33:32 -0600

    Carga de DB

--- Data/StoreContext.cs
+++ Data/StoreContext.cs
@@ -14,5 +14,19 @@ namespace OnlineStore.Data
         public DbSet<User> Users { get; set; }
         public DbSet<CartProduct> CartProducts { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var user1 = new User { Id = 1, Username = "admin", Password = "admin" };
+            modelBuilder.Entity<User>().HasData(user1);
+
+            var p1 = new Product { Id = 1, Name = "Audifonos sony", Price = 10000, Amount = 10 };
+            var p2 = new Product { Id = 2, Name = "Celular samsung", Price = 20000.50M, Amount = 15 };
+            var p3 = new Product { Id = 3, Name = "Jarra", Price = 20, Amount = 20 };
+            var p4 = new Product { Id = 4, Name = "Vaso", Price = 5, Amount = 10 };
+            modelBuilder.Entity<Product>().HasData(p1,p2,p3,p4);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
---
+++ Migrations/20230908181211_SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OnlineStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Amount", "CartId", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 10, null, "Audifonos sony", 10000m },
+                    { 2, 15, null, "Celular samsung", 20000.50m },
+                    { 3, 20, null, "Jarra", 20m },
+                    { 4, 10, null, "Vaso", 5m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Password", "Username" },
+                values: new object[] { 1, "admin", "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
